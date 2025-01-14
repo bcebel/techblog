e8a5407bebdc6c3aa364c9d7a3a929801eb1754c
bcebel <bcebel@gmail.com>
2025-01-13 20:44:53 -0600

    shh

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const topsecret = process.env.secret
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: topsecret,
   cookie: {
     maxAge: 300000,
     httpOnly: true,
